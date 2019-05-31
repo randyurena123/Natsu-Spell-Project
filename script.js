085164e445f32902df0a015ae87be72a99911334
randyurena123 <51236797+randyurena123@users.noreply.github.com>
2019-05-31 09:36:00 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,28 @@
+//Spell #1
+
+$("#SpellButton").click(function(){
+// $("#yellow").fadeOut();
+ $("body").css("background-color","red");
+ $("#text").append("See You Later");
+ $("#yellow").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSICSEUAwnyU-bQG_uuKslxaChkAwkgKNgXAQtIsnxQYQOKLR5p");
+});
+
+
+
+//Spell #2
+
+$("#Natsu").click(function(){
+$("#blue").toggle();
+$("#text#1").append("Bye....Hi");
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
